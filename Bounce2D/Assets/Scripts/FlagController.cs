8d63b21cd4ce394936cbe20c3d95b9d667f7d50e
Pau-Gatell <455837.clot@fje.edu>
2025-11-17 20:19:48 +0100

    Final Changes Bounce

---
+++ Bounce2D/Assets/Scripts/FlagController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FlagController : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            //Només permet guanyar si has agafat tots els checkpoints
+            if (Checkpoint.TotsActivats())
+            {
+                Debug.Log("Has ganado!");
+                SceneManager.LoadScene("WinMenu");
+            }
+            else
+            {
+                Debug.Log("No has activado todos los checkpoints!");
+            }
+        }
+    }
+}
